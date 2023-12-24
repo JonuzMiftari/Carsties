0e892dff1c600333cc61b2bb8761b0ca35a00678
JonuzMiftari <jonuzmiftari@hotmail.com>
2023-12-25 00:04:14 +0100

    End of section 3

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.Services;
+
+namespace SearchService.Data
+{
+    public class DbInitializer
+    {
+        public static async Task InitDb(WebApplication app)
+        {
+            // Init Mongo DB with the connection string
+            await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+            // Create indexes for search
+            await DB.Index<Item>()
+                .Key(x => x.Make, KeyType.Text)
+                .Key(x => x.Model, KeyType.Text)
+                .Key(x => x.Color, KeyType.Text)
+                .CreateAsync();
+
+            using var scope  = app.Services.CreateScope();
+
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.WriteLine($"{items.Count} returned from the auction service");
+
+            if (items.Count > 0) await DB.SaveAsync(items);
+        }
+    }
+}
---
+++ src/SearchService/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Entities;
+using System;
+
+namespace SearchService.Models
+{
+    public class Item : Entity
+    {
+        public int ReservePrice { get; set; }
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public int SoldAmmount { get; set; }
+        public int CurrentHighBid { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+        public DateTime AuctionEnd { get; set; }
+        public string Status { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Mileage { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
